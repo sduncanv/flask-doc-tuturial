b0793651b877bf0a44074e0139aff720e39ad507
Samuel Duncan <srduncanv1217@gmail.com>
2022-11-09 12:47:05 -0500

    Other commit 09/11/2022

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -20,6 +20,13 @@ def create_app(test_config = None):
 
     @app.route('/hello')
     def hello():
-        return 'Hello, world!'
+        return '¡Hello, world!'
     
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
