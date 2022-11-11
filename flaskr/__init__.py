91d9492f333ec0433badf5805b2cacfbe4e8a7bb
Samuel Duncan <srduncanv1217@gmail.com>
2022-11-10 21:12:20 -0500

    third commit 10/11/2022

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -21,7 +21,7 @@ def create_app(test_config = None):
     @app.route('/hello')
     def hello():
     
-        return '¡Hello, world!'
+        return 'Hello, World!'
     
 
     from . import db
--- flaskr/db.py
+++ flaskr/db.py
@@ -31,7 +31,7 @@ def init_db():
         db.executescript(f.read().decode('utf8'))
 
 
-@click.command('init_db')
+@click.command('init-db')
 def init_db_command():
     init_db()
     click.echo('Initialized the database.')
